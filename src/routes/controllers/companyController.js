4fdfb6a1bee9d95a358b5f7b6e374d031c8e9606
luis-sant1 <santiagomuchacholuma@uvm.edu.ve>
2023-11-02 11:04:15 -0600

    FEAT  Controlador de Companys
    - Agregado un pequeño validador de campos.
    - Maneja errores.

--- src/routes/controllers/companyController.js
+++ src/routes/controllers/companyController.js
@@ -9,10 +9,38 @@ const getAllCompanies = async (req, res) => {
 };
 
 const createCompany = async (req, res) => {
-  const newCompany = await prisma.company.create({
-    data: req.body,
-  });
-  res.json(newCompany);
+  try {
+    const { Name, Type, User, Setting, Periodicity, Payroll, Payroll_Employee, Perception, Receipt, Deductions } = req.body
+    if (!Name || !Type) {
+      return res.status(400).json({ error: 'Es necesario rellenar todos los campos para poder avanzar con el registro' });
+    }
+    const existingCompany = await prisma.company.findFirst({
+      where: { Name }
+    });
+    if (existingCompany) {
+      return res.status(400).json({ error: 'Empresa en existencia' });
+    }
+    const newCompany = await prisma.company.create({
+      data: {
+        Name,
+        Type,
+        User,
+        Setting,
+        Periodicity,
+        Payroll,
+        Payroll_Employee,
+        Perception,
+        Receipt,
+        Deductions
+      }
+    });
+    res.status(200).json(newCompany);
+  } catch (error) {
+    console.error('Error al crear empresa:', error);
+        res.status(500).json({
+            error: 'Hubo un error al crear la empresa.',
+        });
+  }
 };
 
 module.exports = { getAllCompanies, createCompany };
