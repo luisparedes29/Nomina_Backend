bacb4bf8aa0f67812583887dcfb87c9e936c783f
luis-sant1 <santiagomuchacholuma@uvm.edu.ve>
2023-11-02 11:04:15 -0600

    FEAT  Controlador de Companys
    - Agregado un pequeño validador de campos.
    - Maneja errores.

--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -4,7 +4,7 @@ const { getAllCompanies, createCompany } = require("./controllers/companyControl
 const {validateToken, checkRole} = require("./controllers/jwtAuth");
 
 router
-  .get("/find/all", validateToken, checkRole("admin"), getAllCompanies) // La protección de ruta con el chequeo del rol de "admin" está aquí solo para probar que funcione.
+  .get("/all", getAllCompanies) // La protección de ruta con el chequeo del rol de "admin" está aquí solo para probar que funcione.
                                                                         // Debe borrarse lueego y proteger las rutas adecuadamente segun los roles que les correspondan
 
   .post("/create", createCompany);
