aae61db213d53e7cc1c1be8ed40d1355a75121a3
luis-sant1 <santiagomuchacholuma@uvm.edu.ve>
2023-11-02 20:48:49 -0600

    FEAT Encontrar una payroll por su id.
    - Añadido una ruta método get que recibe como parámetro un id y encuentra la payroll conrespondiente al id.

--- src/routes/payrollRoutes.js
+++ src/routes/payrollRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createPayroll, getAll } = require('./controllers/payrollControllers');
+const { createPayroll, getAll, getOne } = require('./controllers/payrollControllers');
  
 router
   .post("/create-payroll",  createPayroll) 
   .get("/all",  getAll)
+  .get("/:_id", getOne)
 module.exports = router;
