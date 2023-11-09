d343bf3e423543a1581c67bc24fa4b791854670b
luisparedes29 <luisparedescrv@gmail.com>
2023-11-09 10:45:52 -0400

    Agregadas rutas para traer todos los usuarios y todos los usuarios de una empresa

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, loginUser } = require('./controllers/userController')
+const {
+  registerUser,
+  loginUser,
+  getAllUsers,
+  getAllUsersOfCompany,
+} = require('./controllers/userController')
 
 router
+  .get('/all', getAllUsers)
+  .get('/all-company/:id', getAllUsersOfCompany)
   .post('/signup/:id', registerUser)
 
   .post('/login', loginUser)
