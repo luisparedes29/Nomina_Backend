acebdd9bb42dec0c90c423d23040c0f3109d42f4
luisparedes29 <luisparedescrv@gmail.com>
2023-11-09 08:58:12 -0400

    Terminado crear empleados con arreglos en diversas partes

--- src/routes/employeesRoutes.js
+++ src/routes/employeesRoutes.js
@@ -1,11 +1,17 @@
-const express = require("express");
-const router = express.Router();
-const {createEmployee, allEmployees, getEmployeeById, editEmployee, deleteEmployee} = require('./controllers/employeesController');
- 
+const express = require('express')
+const router = express.Router()
+const {
+  createEmployee,
+  allEmployees,
+  getEmployeeById,
+  editEmployee,
+  deleteEmployee,
+} = require('./controllers/employeesController')
+
 router
-  .post("/create-employee", createEmployee) 
-  .get("/all", allEmployees)
+  .post('/create-employee/:companyId/:departmentId', createEmployee)
+  .get('/all', allEmployees)
   .get('/find-employee/:id', getEmployeeById)
-  .put("/edit-employee/:id", editEmployee)
-  .delete("/delete-employee/:id", deleteEmployee)
-module.exports = router;
+  .put('/edit-employee/:id', editEmployee)
+  .delete('/delete-employee/:id', deleteEmployee)
+module.exports = router
