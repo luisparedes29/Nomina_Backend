d3fdc2dc267e54f3fecc0cf9d72f4d136ef12205
luisparedes29 <luisparedescrv@gmail.com>
2023-11-25 12:18:31 -0400

    Terminado modificacion de superAdmin, terminado Issue de empleados

--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -1,26 +1,25 @@
-const express = require("express");
-const router = express.Router();
-const { getAllCompanies, createCompany, getCompanyById } = require("./controllers/companyController");
-const {validateToken, checkRole} = require("./controllers/jwtAuth");
+const express = require('express')
+const router = express.Router()
+const {
+  getAllCompanies,
+  createCompany,
+  getCompanyById,
+} = require('./controllers/companyController')
+const { validateToken, checkRole } = require('./controllers/jwtAuth')
 
 router
+  .get('/all', validateToken, checkRole(['super-admin']), getAllCompanies)
   .get(
-    "/all",
+    '/find-company/:id',
     validateToken,
-    checkRole(["super-admin"]),
-    getAllCompanies,
-  )
-  .get(
-    "/find-company/:id",
-    validateToken,
-    checkRole(["super-admin", "admin"]),
+    checkRole(['superAdmin', 'admin']),
     getCompanyById
   )
   .post(
-    "/create-company",
+    '/create-company',
     validateToken,
-    checkRole(["super-admin"]),
+    checkRole(['superAdmin']),
     createCompany
-  );
+  )
 
-module.exports = router;
+module.exports = router
