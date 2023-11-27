9ff69700223eb1ed818039e3aa0de2a63fe259a8
gabrielcalderon16 <calderongabriel1603@gmail.com>
2023-11-26 21:55:32 -0400

    lint

--- src/routes/perceptionsRoutes.js
+++ src/routes/perceptionsRoutes.js
@@ -1,32 +1,37 @@
 const express = require("express");
 const router = express.Router();
-const { getPerceptionByEmployee, createPerception, editPerception, deleteOne } = require('./controllers/perceptionsController');
-const {validateToken, checkRole} = require("./controllers/jwtAuth");
+const {
+  getPerceptionByEmployee,
+  createPerception,
+  editPerception,
+  deleteOne,
+} = require("./controllers/perceptionsController");
+const { validateToken, checkRole } = require("./controllers/jwtAuth");
 
 router
   .get(
-    '/find-perceptions/user/:id',
-    validateToken,
-    checkRole(["admin", "user"],
-    getPerceptionByEmployee,
-  )
-  .post(
-    '/create-perception/:companyId/:payrollId/:employeeId',
+    "/find-perceptions/user/:id",
     validateToken,
     checkRole(["admin", "user"]),
-    createPerception,
+    getPerceptionByEmployee
+  )
+  .post(
+    "/create-perception/:companyId/:payrollId/:employeeId",
+    validateToken,
+    checkRole(["admin", "user"]),
+    createPerception
   )
   .put(
-    '/edit-perception/:perceptionId/:employeeId',
+    "/edit-perception/:perceptionId/:employeeId",
     validateToken,
     checkRole(["admin", "user"]),
     editPerception
   )
   .delete(
-    '/delete-perception/:perceptionId/:employeeId',
+    "/delete-perception/:perceptionId/:employeeId",
     validateToken,
     checkRole(["admin", "user"]),
-    deleteOne,
-  )
+    deleteOne
+  );
 
-module.exports = router
+module.exports = router;
