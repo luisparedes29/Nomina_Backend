9c3d33aa1e8c7e25759761b5826d458e3f67bc42
Alexander Avendaño <avendano.ramirez@gmail.com>
2023-12-05 21:14:19 -0400

    Rutas y controladores modificados y agregados

--- src/routes/payrollRoutes.js
+++ src/routes/payrollRoutes.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const router = express.Router()
-const { generatePayroll, getOne } = require('./controllers/payrollControllers')
+const { generatePayroll, getOne, deleteEmployee, deletePayroll, updateStatePayroll, allPayrollsOfCompany } = require('./controllers/payrollControllers')
 const { validateToken, checkRole } = require('./controllers/jwtAuth')
 
 router
-  .get(
-    '/generate-payroll/:companyId/:departmentId',
+  .post(
+    '/generate-payroll/:companyId',
     validateToken,
     checkRole(['admin', 'user']),
     generatePayroll
   )
-  .get('/:_id', validateToken, checkRole(['admin', 'user']), getOne)
+  .get('/all-payrolls/:companyId', validateToken, checkRole(['admin', 'user']), allPayrollsOfCompany)
+  .get('/:id/:companyId', validateToken, checkRole(['admin', 'user']), getOne)
+  .delete('/delete-employee-payroll/:payrollId/:employeeId', validateToken, checkRole(['admin', 'user']), deleteEmployee)
+  .delete('/delete-payroll/:payrollId/:companyId', validateToken, checkRole(['admin', 'user']), deletePayroll)
+  .put('/update-state-payroll/:payrollId/:companyId', validateToken, checkRole(['admin', 'user']), updateStatePayroll)
 module.exports = router
